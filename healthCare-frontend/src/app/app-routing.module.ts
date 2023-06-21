da6053e17f28864b2a1073d6ca8dc44fe7c82581
Gabriel Hawerroth <gabriel.hawerroth04@gmail.com>
2023-06-21 00:01:38 -0300

    criados componentes principais e finalizada nav bar

--- healthCare-frontend/src/app/app-routing.module.ts
+++ healthCare-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { UnidadesComponent } from './components/pages/unidades/unidades.component';
+import { AtendimentosComponent } from './components/pages/atendimentos/atendimentos.component';
+import { PacientesComponent } from './components/pages/pacientes/pacientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'pacientes', component: PacientesComponent },
+  { path: 'unidades', component: UnidadesComponent },
+  { path: 'atendimentos', component: AtendimentosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- healthCare-frontend/src/app/app.module.ts
+++ healthCare-frontend/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { PacientesComponent } from './components/pages/pacientes/pacientes.component';
+import { UnidadesComponent } from './components/pages/unidades/unidades.component';
+import { AtendimentosComponent } from './components/pages/atendimentos/atendimentos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    PacientesComponent,
+    UnidadesComponent,
+    AtendimentosComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
