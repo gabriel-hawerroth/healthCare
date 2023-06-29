5b53c3560d08001af8625985c5bb1a8580230bba
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2023-06-29 12:39:22 -0300

    .

--- healthCare-frontend/src/app/components/pages/pacientes/pacientes.component.ts
+++ healthCare-frontend/src/app/components/pages/pacientes/pacientes.component.ts
@@ -29,11 +29,15 @@ export class PacientesComponent implements OnInit {
       this.allPatients = items;
       this.patients = items;
     });
+
+    // this.buildComponent();
+    //   this.form.valueChanges.pipe(takeUntil(this._unsubscribeAll)).subscribe(() => {this.filterEspecialista()});
+    //   this.atualizaLista();
   }
 
   filterName(e: Event): void {
     const target = e.target as HTMLInputElement;
-    const value = target.value;
+    const value = target.value.toLowerCase();
 
     this.patients = this.allPatients.filter((patient) => {
       return patient.nome.toLowerCase().includes(value);
@@ -48,4 +52,38 @@ export class PacientesComponent implements OnInit {
       return patient.ie_situacao.includes(value);
     });
   }
+
+  // public removeAcentos(newStringComAcento): string {
+  //   if (!newStringComAcento) {
+  //     return "";
+  //   } else if (newStringComAcento === null) {
+  //     alert("Campo descrição nulo");
+  //   }
+
+  //   let str = newStringComAcento;
+
+  //   const mapaAcentosHex = {
+  //     a: /[\xE0-\xE6]/g,
+  //     A: /[\xC0-\xC6]/g,
+  //     e: /[\xE8-\xEB]/g,
+  //     E: /[\xC8-\xCB]/g,
+  //     i: /[\xEC-\xEF]/g,
+  //     I: /[\xCC-\xCF]/g,
+  //     o: /[\xF2-\xF6]/g,
+  //     O: /[\xD2-\xD6]/g,
+  //     u: /[\xF9-\xFC]/g,
+  //     U: /[\xD9-\xDC]/g,
+  //     c: /\xE7/g,
+  //     C: /\xC7/g,
+  //     n: /\xF1/g,
+  //     N: /\xD1/g,
+  //   };
+
+  //   for (const letra in mapaAcentosHex) {
+  //     const expressaoRegular = mapaAcentosHex[letra];
+  //     str = str.replace(expressaoRegular, letra);
+  //   }
+
+  //   return str;
+  // }
 }
