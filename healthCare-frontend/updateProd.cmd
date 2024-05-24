e57fcdbd30cfea4691b065e0be17848355e5e8e7
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-05-23 23:59:10 -0300

    cli: removed update script

--- healthCare-frontend/updateProd.cmd
+++
@@ -1,7 +0,0 @@
-ssh root@15.229.18.114 "pm2 delete ssr.healthcare"
-
-ssh root@15.229.18.114 "rm -rf /var/www/html/healthcare/*"
-
-scp -r dist/front-healthcare/* root@15.229.18.114:/var/www/html/healthcare/
-
-ssh root@15.229.18.114 "/root/start_healthcare.sh"
