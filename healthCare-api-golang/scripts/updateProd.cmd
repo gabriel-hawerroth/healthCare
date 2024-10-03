89ea5b0918e53cdd15f84883902e4fe06c00dc0a
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-10-03 13:44:47 -0300

    ci: updateProd script

--- healthCare-api-golang/scripts/updateProd.cmd
+++ healthCare-api-golang/scripts/updateProd.cmd
@@ -1,27 +1,13 @@
-@echo off
-
-rem 1. Remover todos os arquivos da pasta build
-del /Q "../build/*.*"
-
-rem 2. Definindo env para linux
 set GOOS=linux
 set GOARCH=amd64
 
-rem 3. Entrar na pasta cmd e compilar o código
-cd /D "../cmd"
+cd ../cmd
 go build -ldflags="-linkmode=internal -w -s -extldflags '-static' -X main.BuildCPUFlags=native" -o healthcare_api ./main.go
 
-rem 4. Mover o arquivo de build para a pasta build
-move /Y "healthcare_api" "../build/healthcare_api"
-cd ..
-
-rem 5. Parar a api em produção e remover o arquivo de build
 ssh ubuntu@168.138.150.229 "pm2 delete api.healthcare"
 
-rem 6. Copiar o arquivo de build para a VM em produção
-scp -r ./build/healthcare_api ubuntu@168.138.150.229:/home/ubuntu/prd_projects/back/healthcare/
+scp -r ./healthcare_api ubuntu@168.138.150.229:/home/ubuntu/prd_projects/back/healthcare/
 
-rem 7. Conectar na VM de produção e reiniciar o serviço usando pm2
 ssh ubuntu@168.138.150.229 "/home/ubuntu/start_scripts/start_healthcare_api.sh"
 
-echo Concluído.
+del healthcare_api
--- healthCare-api-golang/scripts/updateProd.sh
+++ healthCare-api-golang/scripts/updateProd.sh
@@ -7,12 +7,10 @@ cd ../cmd
 
 go build -ldflags="-linkmode=internal -w -s -extldflags '-static' -X main.BuildCPUFlags=native" -o healthcare_api ./main.go
 
-mv healthcare_api ../build/
-
-cd ..
-
 ssh ubuntu@168.138.150.229 "pm2 delete api.healthcare"
 
-scp -r ./build/healthcare_api ubuntu@168.138.150.229:/home/ubuntu/prd_projects/back/healthcare/
+scp -r healthcare_api ubuntu@168.138.150.229:/home/ubuntu/prd_projects/back/healthcare/
 
 ssh ubuntu@168.138.150.229 "/home/ubuntu/start_scripts/start_healthcare_api.sh"
+
+rm -rf healthcare_api
