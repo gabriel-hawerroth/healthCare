7197c901c10dfbf74d96a6c031f42a74eeb544dc
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-10-03 13:18:03 -0300

    ci: updated cmd updateProd script

--- healthCare-frontend/updateProd.cmd
+++ healthCare-frontend/updateProd.cmd
@@ -1,9 +1,9 @@
 call npm run build:prod
 
-ssh root@147.79.81.13 "pm2 delete ssr.healthcare"
+ssh ubuntu@168.138.150.229 "pm2 delete ssr.healthcare"
 
-ssh root@147.79.81.13 "rm -rf /var/www/html/healthcare/*"
+ssh ubuntu@168.138.150.229 "rm -rf /home/ubuntu/prd_projects/front/healthcare/*"
 
-scp -r dist/front-healthcare/* root@147.79.81.13:/var/www/html/healthcare/
+scp -r dist/healthcare-frontend/* ubuntu@168.138.150.229:/home/ubuntu/prd_projects/front/healthcare/
 
-ssh root@147.79.81.13 "/root/start_healthcare.sh"
+ssh ubuntu@168.138.150.229 "/home/ubuntu/start_scripts/start_healthcare.sh"
